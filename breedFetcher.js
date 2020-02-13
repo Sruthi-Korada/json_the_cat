abde28a109792a9861a65abc3878e28d2868350a
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-13 23:55:26 +0000

    refactor

---
+++ breedFetcher.js
@@ -0,0 +1,22 @@
+const request = require('request');
+// const breedName = process.argv.slice(2);
+const url = ` https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+const fetchBreedDescription = function(breedName) {
+  request(url, (error, response, body) => {
+    if (error) {
+        return console.log(error);
+      }
+      const data = JSON.parse(body);
+      const breed = data[0];
+      if (breed) {
+        console.log(breed.description);
+      } else {
+        console.log(`Failed to find breed ${breedName}`);
+      }
+  });
+
+};
+
+// fetchBreedDescription(breedName);
+module.exports = {fetchBreedDescription};
