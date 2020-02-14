c52091deb97c9c54c72f3ed62408a8d4e5b6b073
sruthikorad36 <sruthikorada36@gmail.com>
2020-02-14 00:13:43 +0000

    refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,19 @@
 const request = require('request');
-// const breedName = process.argv.slice(2);
+ const breedName = process.argv.slice(2);
 const url = ` https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-const fetchBreedDescription = function(breedName) {
+const fetchBreedDescription = function(breedName, callback) {
   request(url, (error, response, body) => {
     if (error) {
-        return console.log(error);
-      }
-      const data = JSON.parse(body);
-      const breed = data[0];
-      if (breed) {
-        console.log(breed.description);
-      } else {
-        console.log(`Failed to find breed ${breedName}`);
-      }
+      return console.log(error);
+    }
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (breed) {
+      console.log(breed.description);
+    } else {
+      console.log(`Failed to find breed ${breedName}`);
+    }
   });
 
 };
